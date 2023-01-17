e94710fabf3b83aab6b2325e3835b190c34c9eac
STEVEN ALEXANDER GRANT <steven.grant@binus.ac.id>
2023-01-17 21:13:31 +0700

    first commit

---
+++ app/src/main/java/com/binus/grantecom/ChangeProfile.java
@@ -0,0 +1,14 @@
+package com.binus.grantecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+    }
+}
